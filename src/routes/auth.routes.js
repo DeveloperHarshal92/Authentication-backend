1b53d7ed5e9a7b8f3d6c4be12791e00af1f721ec
Harshal Varade <developer.ever@gmail.com>
2026-02-13 10:12:42 +0530

    feat: enhance authentication routes with registration and login functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto")
 
 const authRouter = express.Router();
 
@@ -15,10 +16,12 @@ authRouter.post("/register", async (req, res) => {
     });
   }
 
+  const hash = crypto.createHash("md5").update(password).digest("hex")
+
   const user = await userModel.create({
     name,
     email,
-    password,
+    password : hash,
   });
 
   const token = jwt.sign(
@@ -29,12 +32,52 @@ authRouter.post("/register", async (req, res) => {
     process.env.JWT_SECRET,
   );
 
-  res.cookie("jwt_token",token)
+  res.cookie("jwt_token", token);
 
   res.status(201).json({
     message: "User registered successfully...",
     user,
     token,
+  });
+});
+
+authRouter.post("/protected", (req, res) => {
+  console.log(req.cookies);
+
+  res.status(200).json({
+    message: "This is protected route",
+  });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found with this email address",
+    });
+  }
+
+  const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex");
+  if (!isPasswordMatched) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id, 
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User loggedIn",
+    user,
   });
 });
 
